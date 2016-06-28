f349d96a696b0c487cc3109413f2e0df9f140bdc
Mikhail Korobov <kmike84@gmail.com>
2016-06-28 19:39:26 +0500

    LinkClassifier object which allows to use trained models

---
+++ deep-deep/deepdeep/predictor.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from typing import List, Tuple
+
+import joblib
+import parsel
+from formasaurus.utils import get_domain
+
+from deepdeep.links import extract_link_dicts
+from deepdeep.qlearning import QLearner
+
+
+class LinkClassifier:
+    def __init__(self, Q, link_vectorizer, page_vectorizer, **kwargs):
+        self.Q = Q  # type: QLearner
+        self.link_vectorizer = link_vectorizer
+        self.page_vectorizer = page_vectorizer
+        self.extra = kwargs
+
+    @classmethod
+    def load(cls, path):
+        model = joblib.load(str(path))
+        return cls(**model)
+
+    def extract_urls(self, html: str, url: str) -> List[Tuple[float, str]]:
+        """
+        Extract all URLs from html, return a list of (score, url) pairs.
+        """
+        sel = parsel.Selector(html)
+        links = list(extract_link_dicts(sel, url))
+        for link in links:
+            link['domain_from'] = get_domain(url)
+            link['domain_to'] = get_domain(link['url'])
+
+        page_vec = self.page_vectorizer.transform([html])
+        link_matrix = self.link_vectorizer.transform(links)
+        AS = self.Q.join_As(link_matrix, page_vec)
+        scores = self.Q.predict(AS)
+
+        urls = [link['url'] for link in links]
+        return list(zip(scores, urls))
