b4ff9e66ef43351a7d0d80fcc2606480b84d54ef
Mikhail Korobov <kmike84@gmail.com>
2016-06-28 16:37:05 +0500

    add support for keyword n-grams and for negative keywords

--- deep-deep/deepdeep/spiders/relevancy.py
+++ deep-deep/deepdeep/spiders/relevancy.py
@@ -5,29 +5,11 @@ from pathlib import Path
 from typing import List
 
 from scrapy.http import Response
+from formasaurus.text import tokenize, token_ngrams
 
 from .qspider import QSpider
 from deepdeep.goals import RelevancyGoal
 from deepdeep.utils import html2text
-from formasaurus.text import tokenize
-
-
-def keywords_relevancy(keywords: List[str], response: Response):
-    """
-    Relevancy score based on how many keywords from a list are
-    in response text.
-
-    Score is transformed using a weird log scale (fixme)
-    to *roughly* fit [0,1] interval and to not require all keywords to be
-    present for a page to be relevant.
-    """
-    if not hasattr(response, 'text'):
-        return 0.0
-    text = html2text(response.text).lower()
-    tokens = set(tokenize(text))
-    score = sum(1.0 if keyword in tokens else 0.0 for keyword in keywords)
-    score = math.log(score + 1, len(keywords) / 2)
-    return score
 
 
 class RelevancySpider(QSpider):
@@ -42,7 +24,8 @@ class RelevancySpider(QSpider):
         'max_relevant_pages_per_domain',
     } | QSpider.ALLOWED_ARGUMENTS
     _ARGS = QSpider._ARGS | {
-        'keywords',
+        'pos_keywords',
+        'neg_keywords',
         'discovery_bonus',
         'max_requests_per_domain',
         'max_relevant_pages_per_domain'
@@ -58,7 +41,8 @@ class RelevancySpider(QSpider):
 
     # a file with keywords
     keywords_file = None
-    keywords = []
+    pos_keywords = []
+    neg_keywords = []
 
     custom_settings = {
         # copied from QSpider
@@ -73,11 +57,17 @@ class RelevancySpider(QSpider):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.keywords = Path(self.keywords_file).read_text().split()
+        keywords = Path(self.keywords_file).read_text().splitlines()
+        self.pos_keywords = [k for k in keywords if not k.startswith('-')]
+        self.neg_keywords = [k[1:] for k in keywords if k.startswith('-')]
+        self.max_ngram = _max_ngram_length(self.pos_keywords)
         self._save_params_json()
 
     def relevancy(self, response: Response) -> float:
-        return keywords_relevancy(self.keywords, response)
+        return keywords_response_relevancy(response,
+                                           pos_keywords=self.pos_keywords,
+                                           neg_keywords=self.neg_keywords,
+                                           max_ngram=self.max_ngram)
 
     def get_goal(self):
         self.discovery_bonus = float(self.discovery_bonus)
@@ -91,3 +81,56 @@ class RelevancySpider(QSpider):
             max_requests_per_domain=self.max_requests_per_domain,
             max_relevant_pages_per_domain=self.max_relevant_pages_per_domain,
         )
+
+
+def keywords_response_relevancy(response: Response,
+                                pos_keywords: List[str],
+                                neg_keywords: List[str],
+                                max_ngram=1):
+    """
+    Relevancy score based on how many keywords from a list are
+    in response text.
+
+    Score is transformed using a weird log scale (fixme)
+    to *roughly* fit [0,1] interval and to not require all keywords to be
+    present for a page to be relevant.
+    """
+    if not hasattr(response, 'text'):
+        return 0.0
+    return keyword_relevancy(response.text, pos_keywords, neg_keywords, max_ngram)
+
+
+def keyword_relevancy(response_html: str,
+                      pos_keywords: List[str],
+                      neg_keywords: List[str],
+                      max_ngram=1):
+    text = html2text(response_html).lower()
+    tokens = tokenize(text)
+    tokens = set(token_ngrams(tokens, 1, max_ngram))
+
+    def _score(keywords):
+        s = sum(int(k in tokens) for k in keywords)
+        return _scale_relevancy(s, keywords)
+
+    pos_score = _score(pos_keywords)
+    neg_score = _score(neg_keywords)
+
+    return max(0, pos_score - 0.33 * neg_score)
+
+
+def _max_ngram_length(keywords: List[str]) -> int:
+    """
+    >>> _max_ngram_length(["foo"])
+    1
+    >>> _max_ngram_length(["foo", "foo  bar"])
+    2
+    >>> _max_ngram_length(["  foo", "foo bar", "foo bar baz "])
+    3
+    """
+    return max(len(keyword.split()) for keyword in keywords)
+
+
+def _scale_relevancy(score, keywords):
+    """ Weird log scale to use for keyword occurance count """
+    return math.log(score + 1, len(keywords) / 2 + 2)
+
