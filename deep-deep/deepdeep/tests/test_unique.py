8f122d390843d0071bfbc9a4f7d6a3090d09445e
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-04-20 19:39:55 +0300

    Fix typo in the method name

--- deep-deep/deepdeep/tests/test_unique.py
+++ deep-deep/deepdeep/tests/test_unique.py
@@ -3,12 +3,12 @@ from scrapy.http.response.text import TextResponse  # type: ignore
 from deepdeep.spiders.unique import UniqueContentGoal
 
 
-def text_reponse(text, url='http://example.com'):
+def text_response(text, url='http://example.com'):
     return TextResponse(url, body=text.encode('utf8'), encoding='utf8')
 
 
 def test_unique_goal():
     goal = UniqueContentGoal()
-    assert goal.get_reward(text_reponse('some text')) == 0.03
-    assert goal.get_reward(text_reponse('some text')) == 0.00
-    assert goal.get_reward(text_reponse('some more text')) == 0.04
+    assert goal.get_reward(text_response('some text')) == 0.03
+    assert goal.get_reward(text_response('some text')) == 0.00
+    assert goal.get_reward(text_response('some more text')) == 0.04
